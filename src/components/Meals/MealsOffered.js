406a6fea395e0cdc7cbfb72b8125d897ce0bd808
naji-alhusami <najihussami@gmail.com>
2023-02-21 22:31:43 +0300

    added meals to backend and fetch it

--- src/components/Meals/MealsOffered.js
+++ src/components/Meals/MealsOffered.js
@@ -1,42 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from "../Reusable/Card";
 import Meal from "./Meal";
 
 import classes from "./MealsOffered.module.css";
 
-const OFFERED_MEALS = [
-  {
-    id: "o1",
-    name: "Mushroom Burger",
-    description:
-      "One or more patties made out of mushroom as a replacement, instead of the traditionally used ground meat.",
-    price: 70,
-  },
-  {
-    id: "o2",
-    name: "Neapolitan pizza",
-    description:
-      "Buffalo mozzarella (produced from the milk of Italian Mediterranean buffalo)",
-    price: 105,
-  },
-  {
-    id: "o3",
-    name: "Chicken Curry",
-    description:
-      "Chicken and vegetables, or just vegetables, in a sauce containing hot spices.",
-    price: 90,
-  },
-  {
-    id: "o4",
-    name: "Salad",
-    description:
-      "varieties of lettuce, garden cress and watercress, endives, cabbage, spinach, escarole, romaine (cos), arugula, and fresh herbs.",
-    price: 45,
-  },
-];
-
 const MealsOffered = () => {
-  const allMeals = OFFERED_MEALS.map((meal) => {
+  const [mealsOffered, setMealsOffered] = useState([]);
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://react-hooks-9cae4-default-rtdb.firebaseio.com/meals.json"
+      );
+      const responseData = await response.json();
+      const mealsData = [];
+
+      for (const key in responseData) {
+        mealsData.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+      setMealsOffered(mealsData);
+    };
+    fetchMeals();
+  }, []);
+
+  const allMeals = mealsOffered.map((meal) => {
     return (
       <Meal
         key={meal.id}
